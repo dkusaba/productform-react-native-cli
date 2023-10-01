e7d0a82cd406277cf752cfc64770f887d8881b7e
Dice Kusaba <dicekusaba@gmail.com>
2023-10-01 14:29:54 -0700

    Rename app from 'Practice' to 'Product Form'

--- ios/ProductFormTests/ProductFormTests.m
+++ ios/ProductFormTests/ProductFormTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface practiceTests : XCTestCase
+@interface ProductFormTests : XCTestCase
 
 @end
 
-@implementation practiceTests
+@implementation ProductFormTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
